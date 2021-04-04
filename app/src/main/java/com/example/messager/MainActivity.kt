c5a50e3e9ec5e1d2971deebeb2d991fc51d962bb
Max Cherkasov <maximmc@list.ru>
2021-04-05 01:12:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/messager/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.messager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import com.example.messager.databinding.ActivityMainBinding
+import com.example.messager.ui.fragments.ChatsFragment
+import com.example.messager.ui.objects.AppDrawer
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mToolbar: Toolbar
+    private lateinit var mAppDrawer: AppDrawer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        mAppDrawer.create()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.dataContainer,
+                ChatsFragment()
+            ).commit()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+        mAppDrawer = AppDrawer(this, mToolbar)
+    }
+}
---
+++ app/src/main/java/com/example/messager/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.messager.ui.fragments
+
+import com.example.messager.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
